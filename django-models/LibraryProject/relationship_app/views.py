154e9906b80d997735039d0ffb31fe2a51ec2763
nucleus1073 <abrhamdanayit1995@gmail.com>
2025-03-04 21:33:33 +0300

    gafsh

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book  # Make sure the Book model is imported
 
+# Function-based view to list all books and their authors
 def list_books(request):
-    books = Book.objects.all()  # Retrieve all books from the database
-    return render(request, 'list_books.html', {'books': books})
-from django.views.generic import DetailView
-from .models import Library
+    # Retrieve all books from the database
+    books = Book.objects.all()
+
+    # Render the template and pass the books as context
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 class LibraryDetailView(DetailView):
     model = Library
