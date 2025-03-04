aa94a0b3b3d812ee8bb0b430b0bc98d333ff495b
nucleus1073 <abrhamdanayit1995@gmail.com>
2025-03-04 23:17:45 +0300

    hsnsjs

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,34 +10,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import permission_required,login_required  # Import login_required to restrict access to authenticated users only
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseForbidden
-from django.forms import BookForm # type: ignore
-from django.contrib.auth.decorators import user_passes_test
-
-# Function to check if the user is an Admin
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
-
-# Admin view (only accessible to Admin users)
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-# Librarian view (only accessible to Librarians)
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-# Member view (only accessible to Members)
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
-
+from .forms import BookForm
 
 def list_books(request):
     books = Book.objects.all()
