68b83601e86e724655ff3dab1bd5d37708b9ffa4
Mitochondria <abrhamdanayit1995@gmail.com>
2025-03-12 13:43:26 +0300

    here still

---
+++ api_project/api/settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS = [
+    # Default apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    # Add your app and DRF here
+    'api',  # Your app name
+    'rest_framework',  # Django REST Framework
+]
