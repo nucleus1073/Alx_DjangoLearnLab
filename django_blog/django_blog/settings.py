fa13ec4b6bedc530703d3771f591e34bb47bd35b
Mitochondria <abrhamdanayit1995@gmail.com>
2025-03-23 16:46:40 +0300

    editing the errored files

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,10 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PORT': '',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
